7f3f16c37a297af0f528d7ab65c9a802d17b95c0
Vinit Nakhate <vinitnakhate@gmail.com>
2025-11-28 22:58:34 +0530

    17th Nov 2025

---
+++ src/sample1/demo1.java
@@ -0,0 +1,13 @@
+package sample1;
+
+public class demo1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hi");
+
+        for(int i=1;i<=5;i++)
+        {
+            System.out.println("i = " + i);
+        }
+    }
+}
